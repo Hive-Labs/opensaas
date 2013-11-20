a47f61c2e13223ab80b4adbd0756a94fb9774b20
Rohit Krishnan <rkrishnan@notoja.com>
2013-11-20 10:29:56 -0500

    Added santi to list of allowed g+ users

--- services/authService/db/users.js
+++ services/authService/db/users.js
@@ -1,7 +1,10 @@
 var users = [
     { id: '1', email: 'bob', password: 'secret', displayName: 'Bob Smith' },
     { id: '2', email: 'joe', password: 'password', displayName: 'Joe Davis' },
-    { id: '3', email: 'rohitkrishnan101@gmail.com', password: 'google1', displayName: 'Rohit Krishnan'}
+    { id: '3', email: 'rohitkrishnan101@gmail.com', password: 'google1', displayName: 'Rohit Krishnan'},
+    { id: '4', email: 'sparedes@notoja.com', password: 'google1', displayName: 'Santiago Paredes'},
+    { id: '5', email: 'btran@notoja.com', password: 'google1', displayName: 'Brian Tran'},
+    { id: '6', email: 'tmustafa@notoja.com', password: 'google1', displayName: 'Tanzil Mustafa (Nafiballs)'}
 ];
 
 
