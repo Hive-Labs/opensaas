3fcc1c46e05dce854967ffe4900a72b30c3c66db
Rohit Krishnan <rkrishnan@hivelabs.it>
2014-05-19 22:15:44 -0400

    push

--- services/authService/db/clients.js
+++ services/authService/db/clients.js
@@ -1,27 +1,46 @@
-var clients = [
-    { _id: '1', name: 'Orchestrator Service Management', clientId: 'orchestratorManagement', clientSecret: 'google1' , redirectURI: 'http://heartbeat.hivelabs.it'},
-    { _id: '2', name: 'Orchestrator Service Management', clientId: 'orchestratorManagementLocal', clientSecret: 'google1' , redirectURI: 'http://localhost:3005'},
-    { _id: '3', name: 'Notebook', clientId: 'notebookLocal', clientSecret: 'google1' , redirectURI: 'http://localhost:5000'},
-    { _id: '4', name: 'Notebook', clientId: 'notebook', clientSecret: 'google1' , redirectURI: 'http://notebook.hivelabs.it'}
-];
+var clients = [{
+    _id: '1',
+    name: 'Orchestrator Service Management',
+    clientId: 'orchestratorManagement',
+    clientSecret: 'google1',
+    redirectURI: 'http://heartbeat.hivelabs.it'
+}, {
+    _id: '2',
+    name: 'Orchestrator Service Management',
+    clientId: 'orchestratorManagementLocal',
+    clientSecret: 'google1',
+    redirectURI: 'http://localhost:3005'
+}, {
+    _id: '3',
+    name: 'Notebook',
+    clientId: 'notebookLocal',
+    clientSecret: 'google1',
+    redirectURI: 'http://localhost:5000'
+}, {
+    _id: '4',
+    name: 'Notebook',
+    clientId: 'notebook',
+    clientSecret: 'google1',
+    redirectURI: 'http://notebook.hivelabs.it'
+}];
 
 
 exports.find = function(id, done) {
-  for (var i = 0, len = clients.length; i < len; i++) {
-    var client = clients[i];
-    if (client._id === id) {
-      return done(null, client);
+    for (var i = 0, len = clients.length; i < len; i++) {
+        var client = clients[i];
+        if (client._id === id) {
+            return done(null, client);
+        }
     }
-  }
-  return done(null, null);
+    return done(null, null);
 };
 
 exports.findByClientId = function(clientId, done) {
-  for (var i = 0, len = clients.length; i < len; i++) {
-    var client = clients[i];
-    if (client.clientId === clientId) {
-      return done(null, client);
+    for (var i = 0, len = clients.length; i < len; i++) {
+        var client = clients[i];
+        if (client.clientId === clientId) {
+            return done(null, client);
+        }
     }
-  }
-  return done(null, null);
+    return done(null, null);
 };
