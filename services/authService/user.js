4d3609b7a6cd3da848655cf648b6767642a60653
Rohit Krishnan <rkrishnan@hivelabs.it>
2015-06-04 01:12:46 -0400

    added email field to user

--- services/authService/user.js
+++ services/authService/user.js
@@ -4,12 +4,19 @@
 var passport = require('passport');
 
 exports.info = [
-  passport.authenticate('bearer', { session: false }),
-  function(req, res) {
-    // req.authInfo is set using the `info` argument supplied by
-    // `BearerStrategy`.  It is typically used to indicate scope of the token,
-    // and used in access control checks.  For illustrative purposes, this
-    // example simply returns the scope in the response.
-    res.json({ user_id: req.user._id, displayName: req.user.displayName, scope: req.authInfo.scope });
-  }
+    passport.authenticate('bearer', {
+        session: false
+    }),
+    function(req, res) {
+        // req.authInfo is set using the `info` argument supplied by
+        // `BearerStrategy`.  It is typically used to indicate scope of the token,
+        // and used in access control checks.  For illustrative purposes, this
+        // example simply returns the scope in the response.
+        res.json({
+            user_id: req.user._id,
+            displayName: req.user.displayName,
+            scope: req.authInfo.scope,
+            email: req.user.email
+        });
+    }
 ];
