9f254d251aa07fbdb861e66e96fbcdb06398c230
Rohit Krishnan <rkrishnan@hivelabs.it>
2015-06-25 01:12:22 -0400

    Added local login to jabber

--- services/authService/jabberAuth.js
+++ services/authService/jabberAuth.js
@@ -3,48 +3,48 @@ module.exports = function(db) {
     returnObj.jabberLogin = function(req, res) {
         var jid = req.body.jid;
         var accessToken = req.body.password;
-
-        if (jid.indexOf(".hivelabs.it") >= 0) {
-            db.users.findByEmail(jid, function(err, user) {
-                crypto = require('crypto');
-                var shasum = crypto.createHash('sha1');
-                shasum.update(accessToken);
-                if (user.password != shasum.digest('hex')) {
-                    res.json({
-                        success: false
-                    });
-                } else {
-                    res.json({
-                        success: true
-                    });
-                }
-            });
-        } else {
-            db.accessTokens.find(accessToken, function(err, token) {
-                if (token && !err) {
-                    db.users.find(token.userID, function(err, user) {
-                        if (!error && user) {
-                            console.log("Jabber auth success");
-                            res.json({
-                                success: true
-                            });
-                        } else {
-                            console.log("Invalid user.");
-                            console.log(err);
+        db.accessTokens.find(accessToken, function(err, token) {
+            if (token && !err) {
+                db.users.find(token.userID, function(err, user) {
+                    if (!error && user) {
+                        console.log("Jabber auth success");
+                        res.json({
+                            success: true
+                        });
+                    } else {
+                        console.log("Invalid user.");
+                        console.log(err);
+                        res.json({
+                            success: false
+                        });
+                    }
+                });
+            } else {
+                db.users.findByEmail(jid + "@hivelabs.it", function(err, user) {
+                    if (err || !user) {
+                        console.log("Invalid token.");
+                        console.log(err);
+                        res.json({
+                            success: false
+                        });
+                    } else {
+                        crypto = require('crypto');
+                        var shasum = crypto.createHash('sha1');
+                        shasum.update(accessToken);
+                        if (user.password != shasum.digest('hex')) {
+                            console.log("Invalid password.");
                             res.json({
                                 success: false
                             });
+                        } else {
+                            res.json({
+                                success: true
+                            });
                         }
-                    });
-                } else {
-                    console.log("Invalid token.");
-                    console.log(err);
-                    res.json({
-                        success: false
-                    });
-                }
-            });
-        }
+                    }
+                });
+            }
+        });
     }
     return returnObj;
 }
